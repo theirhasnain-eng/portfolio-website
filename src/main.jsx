f5af20da72b077d68e00ea395ecbe15c1bed9cfe
theirhasnain <theirhasnain@gmail.com>
2026-06-22 04:06:24 -0700

    vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,15 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/react";  // add this
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+    <Analytics />  {/* add this */}
+  </StrictMode>
+);
